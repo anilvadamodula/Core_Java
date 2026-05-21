9bd55559a8e07f121bfad5f1225de8c49503abdd
Anil <anilvadamodula@gmail.com>
2026-05-21 14:37:11 +0530

    second commit

--- ArrayList/Program1.java
+++ ArrayList/Program1.java
@@ -1,5 +1,19 @@
 package ArrayList;
 
-public class Program1 {
+import java.util.ArrayList;
 
+public class Program1 {
+      public static void main(String args[]){
+          ArrayList<Integer> arr = new ArrayList<>();
+          arr.add(10);
+          arr.add(20);
+          arr.add(30);
+          arr.add(40);
+          arr.add(50);
+
+          for(Integer it : arr)
+          {
+              System.out.print(it+" ");
+          }
+      }
 }
